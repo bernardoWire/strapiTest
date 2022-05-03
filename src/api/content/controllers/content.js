7d55256fd5beabefa7442f774deb662da5a631e3
Bernardo Moya <bernardo.moya@wirewheel.io>
2022-05-03 08:00:56 -0500

    Feat: User separate info

---
+++ src/api/content/controllers/content.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ *  content controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::content.content", ({ strapi }) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx, {
+      populate: {
+        client: true,
+      },
+    });
+
+    const userId = ctx.state.user.id;
+    return data.filter(
+      (content) => content.attributes.client.data.id === userId
+    );
+
+    return { data, meta };
+  },
+}));
