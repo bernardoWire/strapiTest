541c73435a0a342d7803478569679b8c79ca44ea
Bernardo Moya <bernardo.moya@wirewheel.io>
2022-05-06 13:04:15 -0500

    Feat: populate client with full content and cutomizations

--- src/api/client/controllers/client.js
+++ src/api/client/controllers/client.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  *  client controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::client.client');
+module.exports = createCoreController("api::client.client", {
+  async find(ctx) {
+    console.log("HERE");
+    ctx.query = {
+      ...ctx.query,
+      populate: ["content.logo", "customizations"],
+    };
+
+    const { data } = await super.find(ctx);
+
+    return data;
+  },
+  async findOne(ctx) {
+    console.log("HERE");
+    ctx.query = {
+      ...ctx.query,
+      populate: ["content.logo", "customizations"],
+    };
+
+    const { data } = await super.findOne(ctx);
+
+    return data;
+  },
+});
